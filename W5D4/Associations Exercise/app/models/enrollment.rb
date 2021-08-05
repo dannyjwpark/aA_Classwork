0d287dcabc29cd769b443d88f56fd2f95fe99ab1
Mauricio <mjlomeli@uci.edu>
2021-08-05 14:21:15 -0700

    tried user has_many

--- W5D4/Associations Exercise/app/models/enrollment.rb
+++ W5D4/Associations Exercise/app/models/enrollment.rb
@@ -4,6 +4,15 @@ class Enrollment < ApplicationRecord
         foreign_key: :course_id,
         class_name: :Course
     })
+
+    has_many(:user, {
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
+    })
+
+    
+
 end
 
 
