6d92b307d8d727bcd2febb8b76f767c9148470d8
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-11 15:23:15 -0700

    progress

--- W6D3/routes_v2/app/controllers/users_controller.rb
+++ W6D3/routes_v2/app/controllers/users_controller.rb
@@ -7,18 +7,20 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
-        if @user.save
-            render json: @user
-        else
-            render json: @user.errors.full_messages, status: unprocessable_entity
-        end
+        # @user = User.new(user_params)
+        # if @user.save
+        #     render json: @user
+        # else
+        #     render json: @user.errors.full_messages, status: unprocessable_entity
+        # end
+        render json: params
     end
 
     def new
     end
 
     def show
+        render json: User.find()
     end
 
     def update
