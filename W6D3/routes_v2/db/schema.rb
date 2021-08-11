1d57d39ca3cad3347a8620414c09ffbeab64f32f
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-11 16:48:34 -0700

    starting art share

--- W6D3/routes_v2/db/schema.rb
+++ W6D3/routes_v2/db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_230806) do
+ActiveRecord::Schema.define(version: 2021_08_11_233600) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
+    t.string "username", null: false
   end
 
 end
