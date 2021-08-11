1e0e1a81a4bb9e603dc0dcfe0454cbdce306fceb
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-11 16:17:05 -0700

    model done

--- W6D3/routes_v2/app/controllers/users_controller.rb
+++ W6D3/routes_v2/app/controllers/users_controller.rb
@@ -1,19 +1,17 @@
 class UsersController < ApplicationController
 
     def index
-        # @user = User.all
-        # render json: @user
-        render plain: "I'm in the index action!"
+        @user = User.all
+        render json: @user
     end
 
     def create
-        # @user = User.new(user_params)
-        # if @user.save
-        #     render json: @user
-        # else
-        #     render json: @user.errors.full_messages, status: unprocessable_entity
-        # end
-        render json: params
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: unprocessable_entity
+        end
     end
 
     def new
--- W6D3/routes_v2/db/schema.rb
+++ W6D3/routes_v2/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_214948) do
+ActiveRecord::Schema.define(version: 2021_08_11_230806) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
+    t.string "name", null: false
     t.string "email", null: false
   end
 
