cec11e4444794bd5d64f908cf38e7ad9d849c5ab
Danny Park <danincali@gmail.com>
2021-08-12 15:18:38 -0700

    phase 3 ;last

--- W6D3/routes_v2/config/routes.rb
+++ W6D3/routes_v2/config/routes.rb
@@ -20,7 +20,9 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :destroy, :index, :show, :update ]
+  resources :users, only: [:create, :destroy, :index, :show, :update ] do
+    resources :artworks, only: [:index]
+  end
   
   resources :artwork_shares, only: [:create, :destroy]
 
