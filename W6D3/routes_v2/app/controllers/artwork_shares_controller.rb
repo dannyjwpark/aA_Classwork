05d65075f82dae9b7f3239b93e5f2f7e37a826d1
Danny Park <danincali@gmail.com>
2021-08-12 10:27:29 -0700

    routes_phase3_v2

--- W6D3/routes_v2/app/controllers/artwork_shares_controller.rb
+++ W6D3/routes_v2/app/controllers/artwork_shares_controller.rb
@@ -1,5 +1,19 @@
 class ArtworkSharesController < ApplicationController
 
+    def create
+        artwork_share= ArtworkShare.new(artwork_shares_params)
+        if artwork_share.save
+            redirect_to artwork_shares_url(artwork_share)
+        else
+            render json: artwork_share.errors.full_messages, status: unprocessable_entity
+        end
+    end
+
+    private
+    def artwork_shares_params
+        params.require(:artwork_share).permit(:artwork_id, :viewer_id)
+
+    end
     
 
 end
