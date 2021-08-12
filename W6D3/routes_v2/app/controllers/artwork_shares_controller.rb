0046a04d0b9bf1540b7ed40a8e4334a676314de9
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-12 10:10:56 -0700

    shares added

---
+++ W6D3/routes_v2/app/controllers/artwork_shares_controller.rb
@@ -0,0 +1,5 @@
+class ArtworkSharesController < ApplicationController
+
+    
+
+end
--- W6D3/routes_v2/config/routes.rb
+++ W6D3/routes_v2/config/routes.rb
@@ -6,16 +6,20 @@
 # PUT    /users/:id(.:format)                    users#update
 # DELETE /users/:id(.:format)                    users#destroy
 
+
+# artwork_shares POST   /artwork_shares(.:format)                                              artwork_shares#create
+# artwork_share DELETE /artwork_shares/:id(.:format)                                           artwork_shares#destroy
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :destroy, :index, :show, :update ]
   
+  resources :artwork_shares, only: [:create, :destroy]
 
 
 
   # get '/users', to: 'users#index' # as: 'user'
   # post '/users', to: 'users#create'
-  # get '/users/new', to: 'users#new'
   # get '/users/:id', to: 'users#show'
   # patch '/users/:id', to: 'users#update'
   # delete '/users/:id', to: 'users#destroy'
