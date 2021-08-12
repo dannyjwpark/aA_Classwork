b2cf6640b707d6121801f178e9c329976b79bf2a
Danny Park <danincali@gmail.com>
2021-08-11 17:04:53 -0700

    Art Share API v1

--- W6D3/routes_v2/db/schema.rb
+++ W6D3/routes_v2/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_233600) do
+ActiveRecord::Schema.define(version: 2021_08_11_235448) do
+
+  create_table "artwork_shares", force: :cascade do |t|
+    t.integer "artwork_id", null: false
+    t.integer "viewer_id", null: false
+    t.index ["artwork_id"], name: "index_artwork_shares_on_artwork_id", unique: true
+    t.index ["viewer_id"], name: "index_artwork_shares_on_viewer_id"
+  end
+
+  create_table "artworks", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "image_url", null: false
+    t.integer "artist_id", null: false
+    t.index ["artist_id"], name: "index_artworks_on_artist_id"
+    t.index ["title", "artist_id"], name: "index_artworks_on_title_and_artist_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
