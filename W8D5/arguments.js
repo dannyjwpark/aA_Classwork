f8a66bffcc33993f84ca9f44dc2602737d10da87
josephafern <60451011+josephafern@users.noreply.github.com>
2021-08-27 15:17:02 -0700

    Completed arguments.js

--- W8D5/arguments.js
+++ W8D5/arguments.js
@@ -29,18 +29,24 @@ class Dog {
 }
 
 // with arguments keyword
-Function.prototype.myBind = function(ctx){
-    const that = this;
-    const arr = Array.from(arguments).slice(1);
-    // console.log("this is the arg array:" + arr);
-    return function(){
-        let callArgs = Array.from(arguments);
-        // console.log("this is the callArgs:" + callArgs);
-        return that.apply(ctx, arr.concat(callArgs));
-    }
-}
+// Function.prototype.myBind = function(ctx){
+//     const that = this;
+//     const arr = Array.from(arguments).slice(1);
+//     // console.log("this is the arg array:" + arr);
+//     return function(){
+//         let callArgs = Array.from(arguments);
+//         // console.log("this is the callArgs:" + callArgs);
+//         return that.apply(ctx, arr.concat(callArgs));
+//     }
+// }
 
 // with ... rest operator
+Function.prototype.myBind = function(ctx, ...args){
+    const that = this;
+    return function(...callArgs){
+        return that.apply(ctx, args.concat(callArgs));
+    }
+}
 
 const markov = new Cat("Markov");
 const pavlov = new Dog("Pavlov");
@@ -69,3 +75,49 @@ const notMarkovSays = markov.says.myBind(pavlov);
 notMarkovSays("meow", "me");
 // Pavlov says meow to me!
 // true
+
+
+
+function sumThree(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+  
+// Function.prototype.curry = function(num){
+//     const numbers = [];
+//     const that = this;
+//     return function _curriedSum(number){
+//         numbers.push(number);
+//         if (num === numbers.length){
+//             return that.apply(this, numbers);
+//         } else {
+//             return _curriedSum;
+//         } 
+//     }
+// }
+
+Function.prototype.curry = function(num){
+    const numbers = [];
+    const that = this;
+    return function _curriedSum(number){
+        numbers.push(number);
+        if (num === numbers.length){
+            return that.call(this, ...numbers);
+        } else {
+            return _curriedSum;
+        } 
+    }
+}
+
+console.log(sumThree(4, 21, 6)); // == 30
+  
+// you'll write `Function#curry`!
+let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+f1 = f1(4); // [Function]
+f1 = f1(21); // [Function]
+f1 = f1(6); // = 30
+console.log(f1);
+// or more briefly:
+console.log(sumThree.curry(3)(4)(21)(6)); // == 30
+
+
+
