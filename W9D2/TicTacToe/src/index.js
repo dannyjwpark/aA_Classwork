d07ef4e6eca45d253cd5c21cfacd4cd7db0bae6b
Danny Park <danincali@gmail.com>
2021-08-31 14:21:02 -0700

    tictactoe v1

---
+++ W9D2/TicTacToe/src/index.js
@@ -0,0 +1,10 @@
+const View = require("../dist/ttt.css");  // require appropriate file
+const Game = require("../ttt_node/game.js");  // require appropriate file
+
+window.View = View;
+window.Game = Game;
+
+document.addEventListener("DOMContentLoaded", () => {
+  // find the container element in the view created in dist/index.html
+
+});
