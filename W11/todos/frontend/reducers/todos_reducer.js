64bf736a2813c7f6aaec1e054d616b2fc9442683
Danny Park <danincali@gmail.com>
2021-09-14 15:36:11 -0700

    todos + steps actions & reducers  done

--- W11/todos/frontend/reducers/todos_reducer.js
+++ W11/todos/frontend/reducers/todos_reducer.js
@@ -1,20 +1,48 @@
-import {RECEIVE_TODO, RECEIVE_TODOS} from "../actions/todo_actions";
+import {RECEIVE_TODO, RECEIVE_TODOS, REMOVE_TODO} from "../actions/todo_actions";
 
-
-
-export const todosReducer = (oldState, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+export const todosReducer = (oldState=initialState, action) => {
   Object.freeze(oldState);
   const newState = Object.assign({}, oldState);
   switch (action.type) {
     case RECEIVE_TODO:
       newState[action.todo.id] = action.todo;
       return newState;
+
     case RECEIVE_TODOS:
-      newState[action.todos.id] = action.todos;
+      for(const property in action.todos){
+        // if buggy, revisit here
+        newState[action.todos[property].id] = action.todos[property];
+        // ***
+      }
+    return newState;
+    
+    case REMOVE_TODO:
+      delete newState[action.todo.id];
       return newState;
+
     default:
       return newState;
   }
 };
 
 export default todosReducer;
+
+
+// testing
+// store.dispatch(toremoveAction({ id: 1 }))
+
+
--- W11/todos/frontend/todo_redux.jsx
+++ W11/todos/frontend/todo_redux.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import configureStore from "./store/store";
-import {receiveTodo} from "./actions/todo_actions";
+import {receiveTodo, receiveTodos, removeTodo} from "./actions/todo_actions";
+import { receiveSteps, receiveStep, removeStep } from "./actions/step_actions";
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const store = configureStore();
     window.todoAction = receiveTodo;
+    window.todosAction = receiveTodos;
+    window.todoremoveAction = removeTodo;
+    window.stepsAction = receiveSteps;
+    window.stepAction = receiveStep;
+    window.stepremoveAction = removeStep;
     window.store = store;
     ReactDOM.render(<h1>Todos App</h1>, document.getElementById("root"))
 })
