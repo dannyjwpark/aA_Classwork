080bc63385eb65bafa9e28a465ddd4c05d520d47
Danny Park <danincali@gmail.com>
2021-09-17 17:01:48 -0700

    left off at pokemonReducer

--- W11D5/Pokedox/frontend/actions/pokemon_actions.js
+++ W11D5/Pokedox/frontend/actions/pokemon_actions.js
@@ -1,1 +1,9 @@
-export const RECEIVE_ALL_POKEMON = "REACEIVE_ALL_POKEMON";
+// frontend/actions/pokemon_actions.js
+
+export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON';
+
+export const receiveAllPokemon = pokemon => ({
+    type: RECEIVE_ALL_POKEMON,
+    pokemon // received @ reducer case
+})
+
--- W11D5/Pokedox/frontend/pokedex.jsx
+++ W11D5/Pokedox/frontend/pokedex.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM  from "react-dom";
+import {receiveAllPokemon} from "./actions/pokemon_actions"
+import {fetchAllPokemon} from "./util/api_util"
 
 document.addEventListener("DOMContentLoaded",()=>{
-  const root = document.getElementById("root")
+  const root = document.getElementById("root")  
+
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
 
   console.log("React is working");
   ReactDOM.render(<h1>Pokedex</h1>,root);
-
+  
 })
