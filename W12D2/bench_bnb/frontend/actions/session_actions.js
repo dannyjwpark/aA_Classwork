a807da2fde58dbe872918e54f0c9e11d2d7a7af4
Alex Crooks <alexcrooks2@gmail.com>
2021-09-21 17:13:18 -0700

    Reducers galore

--- W12D2/bench_bnb/frontend/actions/session_actions.js
+++ W12D2/bench_bnb/frontend/actions/session_actions.js
@@ -1,12 +1,42 @@
-import { postUser, postSession, deleteSession } from "../util/session_api_util";
+import {
+    postUser,
+    deleteSession,
+    postSession
+} from '../utils/session';
 
-// login(user) (thunk action creator)
-export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS"
 
 
-export const login = (user)
+// action creators
+const receiveCurrentUser = user =>({
+    // POJO 
+    type: RECEIVE_CURRENT_USER,
+    user,
+});
+
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER
+})
 
 
+// thunk action
+export const signup = formUser => dispatch => postUser(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const login = formUser => dispatch => postSession(formUser)
+    // upon successful login, take a user object & dispatch receiveCurrentUser
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => deleteSession()
+    // upon succesful logout, dispatch logoutCurrentUser  
+    .then(() => dispatch(logoutCurrentUser()));
+
+export const receiveErrors = (errors) => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+})
 // logout() (thunk action creator)
 // signup(user) (thunk action creator)
 // receiveCurrentUser(currentUser) (regular action creator)
